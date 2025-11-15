d35a6b063b318d61010862132a681e90598d9199
omdaniel <macbookpro@MacBook-Pro.local>
2025-11-14 22:13:29 -0500

    deleted orphaned legacy files

--- tmp_test.rs
+++
@@ -1,9 +0,0 @@
-use winit::event_loop::{ActiveEventLoop, EventLoop};
-use winit::window::Window;
-
-struct Foo<'a> {
-    window: Option<Window>,
-    surface: Option<wgpu::Surface<'a>>,
-}
-
-fn main() {}
